d88db519eac6993a1cf94233f54830afb5129040
Van Staub <van_staub@us.ibm.com>
2018-09-13 13:39:39 -0400

    target es2017

--- dist/SqlQuery.js
+++ dist/SqlQuery.js
@@ -1,12 +1,4 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 const rp = require("request-promise");
 const xml2js = require("xml2js");
@@ -26,7 +18,8 @@ class SqlQuery {
         this.options = {
             url,
             headers: {
-                accept: 'application/json'
+                accept: 'application/json',
+                'User-Agent': 'openwhisk-sql-query'
             },
             auth: {
                 bearer: token
@@ -34,15 +27,13 @@ class SqlQuery {
             json: true,
         };
     }
-    getCosKey(endpoint, auth, bucket, prefix) {
-        return __awaiter(this, void 0, void 0, function* () {
-            const response = yield rp({
-                url: `https://${endpoint}/${bucket}?prefix=${encodeURIComponent(prefix)}`,
-                auth,
-            });
-            return new Promise((resolve, reject) => {
-                xml2js.parseString(response, (err, result) => err ? reject(err) : resolve(result));
-            });
+    async getCosKey(endpoint, auth, bucket, prefix) {
+        const response = await rp({
+            url: `https://${endpoint}/${bucket}?prefix=${encodeURIComponent(prefix)}`,
+            auth,
+        });
+        return new Promise((resolve, reject) => {
+            xml2js.parseString(response, (err, result) => err ? reject(err) : resolve(result));
         });
     }
     runSqlJob(statement, resultset_target) {
@@ -58,53 +49,47 @@ class SqlQuery {
             }
         }));
     }
-    getSqlJob(jobId) {
-        return __awaiter(this, void 0, void 0, function* () {
-            const response = yield rp(Object.assign({}, this.options, {
-                url: `${this.endpoint}/sql_jobs/${jobId}?instance_crn=${this.instance_crn}`
-            }));
-            const [endpoint, bucket, ...rest] = response.resultset_location.substring(6).split('/');
-            let key = rest.join('/');
-            const cos = yield this.getCosKey(endpoint, this.options.auth, bucket, `${key}/part`);
-            key = cos.ListBucketResult.Contents[0].Key[0];
-            return Object.assign({}, response, { endpoint, bucket, key });
-        });
+    async getSqlJob(jobId) {
+        const response = await rp(Object.assign({}, this.options, {
+            url: `${this.endpoint}/sql_jobs/${jobId}?instance_crn=${this.instance_crn}`
+        }));
+        const [endpoint, bucket, ...rest] = response.resultset_location.substring(6).split('/');
+        let key = rest.join('/');
+        const cos = await this.getCosKey(endpoint, this.options.auth, bucket, `${key}/part`);
+        key = cos.ListBucketResult.Contents[0].Key[0];
+        return Object.assign({}, response, { endpoint, bucket, key });
     }
-    getSqlJobs() {
-        return __awaiter(this, void 0, void 0, function* () {
-            return rp(this.options);
-        });
+    async getSqlJobs() {
+        return rp(this.options);
     }
 }
 exports.SqlQuery = SqlQuery;
-function main(params) {
-    return __awaiter(this, void 0, void 0, function* () {
-        const { endpoint = 'https://sql-api.ng.bluemix.net/v2', instance_crn, apiKey, statement, resultset_target, job_id } = params;
-        let { token } = params;
-        // apiKey provided - get IAM token
-        if (apiKey) {
-            const response = yield rp({
-                url: 'https://iam.bluemix.net/identity/token',
-                headers: {
-                    'Content-Type': 'application/x-www-form-urlencoded',
-                },
-                method: "POST",
-                body: `apikey=${apiKey}&grant_type=urn%3Aibm%3Aparams%3Aoauth%3Agrant-type%3Aapikey`,
-                json: true
-            });
-            token = response.access_token;
-        }
-        const sqlQuery = new SqlQuery(instance_crn, token, endpoint);
-        // user provided a statement - intent is to run SQL Query job
-        if (statement) {
-            return sqlQuery.runSqlJob(statement, resultset_target);
-        }
-        // user provided a job ID - intent is to get a SQL Query job
-        if (job_id) {
-            return sqlQuery.getSqlJob(job_id);
-        }
-        // assume the intent is the list of recent jobs
-        return sqlQuery.getSqlJobs();
-    });
+async function main(params) {
+    const { endpoint = 'https://sql-api.ng.bluemix.net/v2', instance_crn, apiKey, statement, resultset_target, job_id } = params;
+    let { token } = params;
+    // apiKey provided - get IAM token
+    if (apiKey) {
+        const response = await rp({
+            url: 'https://iam.bluemix.net/identity/token',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            method: "POST",
+            body: `apikey=${apiKey}&grant_type=urn%3Aibm%3Aparams%3Aoauth%3Agrant-type%3Aapikey`,
+            json: true
+        });
+        token = response.access_token;
+    }
+    const sqlQuery = new SqlQuery(instance_crn, token, endpoint);
+    // user provided a statement - intent is to run SQL Query job
+    if (statement) {
+        return sqlQuery.runSqlJob(statement, resultset_target);
+    }
+    // user provided a job ID - intent is to get a SQL Query job
+    if (job_id) {
+        return sqlQuery.getSqlJob(job_id);
+    }
+    // assume the intent is the list of recent jobs
+    return sqlQuery.getSqlJobs();
 }
 exports.default = main;
